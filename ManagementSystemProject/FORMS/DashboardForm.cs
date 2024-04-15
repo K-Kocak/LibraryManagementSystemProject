5ca0cc323277df156bcc1b7e782a8b1bd3b165d8
K-Kocak <NA>
2024-04-15 21:55:23 +0100

    Notes for DashboardForm

--- ManagementSystemProject/FORMS/DashboardForm.cs
+++ ManagementSystemProject/FORMS/DashboardForm.cs
@@ -1,13 +1,8 @@
-﻿using ManagementSystemProject.CLASSES;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
 using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace ManagementSystemProject.FORMS
@@ -21,6 +16,7 @@ namespace ManagementSystemProject.FORMS
 
         private void DashboardForm_Load(object sender, EventArgs e)
         {
+            // Loads the images contained on the Dashboard Form
             pictureCloseButton.Image = Image.FromFile("../../IMAGES/closebuttonimage2.png");
             bookButton.Image = Image.FromFile("../../IMAGES/bookimage.png");
             authorButton.Image = Image.FromFile("../../IMAGES/penimage.png");
@@ -32,28 +28,34 @@ namespace ManagementSystemProject.FORMS
             
         }
 
+        
         CLASSES.BOOKS book = new CLASSES.BOOKS();
         CLASSES.AUTHOR author = new CLASSES.AUTHOR();
         CLASSES.MEMBER member = new CLASSES.MEMBER();
         private void pictureCloseButton_Click(object sender, EventArgs e)
         {
+            // Close the window if the X button is pressed in the top right
             Application.Exit();
         }
 
         private void DashboardForm_Shown(object sender, EventArgs e)
         {
+            // Upon loading dashboard form, it will disable the window and pop up the login form (to log in)
             this.Enabled = false;
             LoginForm lgf = new LoginForm(this);
             lgf.Show();
 
+            // These are to display the numbers for total Books, Authors and Members in the database
             bookCountDash.Text = book.BookList().Rows.Count.ToString();
             authorCountDash.Text = author.AuthorList(false).Rows.Count.ToString();
             memberCountDash.Text = member.MemberList(false).Rows.Count.ToString();
 
+            // Grabs all the books in the database
             DataTable bookData = book.BookList();
             byte[] img;
             MemoryStream ms;
             int i = 0;
+            // This for loop generates the books in the "latest books" section
             foreach (var panelControl in panel7.Controls)
             {
                 if(panelControl.GetType() == typeof(Panel))
@@ -61,9 +63,12 @@ namespace ManagementSystemProject.FORMS
                     Panel panel = (Panel) panelControl;
                     img = (byte[])bookData.Rows[i][10];
                     ms = new MemoryStream(img);
+                    // Sets the image
                     panel.BackgroundImage = Image.FromStream(ms);
+                    // Makes it scale to the panel
                     panel.BackgroundImageLayout = ImageLayout.Stretch;
 
+                    // Sets the £ to the book's price
                     foreach(var labelControl in panel.Controls)
                     {
                         if(labelControl.GetType() == typeof(Label))
@@ -85,6 +90,7 @@ namespace ManagementSystemProject.FORMS
 
         private void genreButton_Click(object sender, EventArgs e)
         {
+            // Upons the genre form
             SelectedButton(genreButton);
             ManageGenresForm mngGnrF = new ManageGenresForm();
             mngGnrF.Show();
@@ -92,6 +98,7 @@ namespace ManagementSystemProject.FORMS
 
         private void authorButton_Click(object sender, EventArgs e)
         {
+            // Opens the author form
             SelectedButton(authorButton);
             ManageAuthorsForm mngAutF = new ManageAuthorsForm();
             mngAutF.Show();
@@ -99,6 +106,7 @@ namespace ManagementSystemProject.FORMS
 
         private void bookButton_Click(object sender, EventArgs e)
         {
+            // Opens the "books" form
             SelectedButton(bookButton);
             ManageBooksForm mngBookF = new ManageBooksForm();
             mngBookF.Show();
@@ -106,6 +114,7 @@ namespace ManagementSystemProject.FORMS
 
         private void memberButton_Click(object sender, EventArgs e)
         {
+            // Opens the "Members" form
             SelectedButton(memberButton);
             ManageMembersForm mngMemF = new ManageMembersForm();
             mngMemF.Show();
@@ -113,6 +122,7 @@ namespace ManagementSystemProject.FORMS
 
         public void SelectedButton(Button btn)
         {
+            // Changes the colour of a button when you click on it, while changing the OTHER buttons to a different colour
             foreach(Control control in panelMenu.Controls)
             {
                 if(control.GetType() == typeof(Button))
@@ -128,6 +138,7 @@ namespace ManagementSystemProject.FORMS
 
         private void circulationButton_Click(object sender, EventArgs e)
         {
+            // Circulation form
             SelectedButton(circulationButton);
             ManageCirculationForm mngCirF = new ManageCirculationForm();
             mngCirF.Show();
@@ -135,6 +146,7 @@ namespace ManagementSystemProject.FORMS
 
         private void userButton_Click(object sender, EventArgs e)
         {
+            // Users form
             SelectedButton(userButton);
             ManageUserForm MngUsF = new ManageUserForm();
             MngUsF.Show();
