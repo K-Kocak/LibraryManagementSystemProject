56a22cb59e47e85ae3f64add548e27be906ba242
K-Kocak <NA>
2024-04-16 17:52:32 +0100

    LoginForm Notes

--- ManagementSystemProject/FORMS/LoginForm.cs
+++ ManagementSystemProject/FORMS/LoginForm.cs
@@ -20,6 +20,7 @@ namespace ManagementSystemProject.FORMS
 
         private void LoginForm_Load(object sender, EventArgs e)
         {
+            // Loads images
             pictureUser.Image = Image.FromFile("../../IMAGES/userimage.png");
             pictureUser.SizeMode = PictureBoxSizeMode.Zoom;
             pictureLock.Image = Image.FromFile("../../IMAGES/lockimage.png");
@@ -28,16 +29,18 @@ namespace ManagementSystemProject.FORMS
 
         private void closeLoginWindow_MouseEnter(object sender, EventArgs e)
         {
+            // Changes the X button (for closing the window) to white when the mouse hovers over it
             closeLoginWindow.ForeColor = Color.White;
         }
 
         private void closeLoginWindow_MouseLeave(object sender, EventArgs e)
         {
+            // Back to black when mouse no longer hovers over it
             closeLoginWindow.ForeColor = Color.Black;
         }
 
         private void closeLoginWindow_Click(object sender, EventArgs e)
-        {
+        {          
             Application.Exit();
         }
 
@@ -50,12 +53,14 @@ namespace ManagementSystemProject.FORMS
         private void buttonLogin_Click(object sender, EventArgs e)
         {
             
-
+            // Upon clicking login, creates a db object
             THE_DATABASE.MYDB db = new THE_DATABASE.MYDB();
 
+            // grabs entered username + password
             string username = textboxUsername.Text;
             string password = textboxPassword.Text;
 
+            // looks for the user in the database
             DataTable table = new DataTable();
             MySqlDataAdapter adapter = new MySqlDataAdapter();
             MySqlCommand command = new MySqlCommand("SELECT * FROM `app_users` WHERE `username`=@usn AND `password`=@pass", db.getConnection());
@@ -69,17 +74,20 @@ namespace ManagementSystemProject.FORMS
             {
                 if (table.Rows[0][5].ToString().Equals("owner"))
                 {
+                    // if the user found is an owner, enables the dashboard and closes login form
                     dashF.Enabled = true;
                     this.Close();
                 }
                 else if (table.Rows[0][5].ToString().Equals("admin"))
                 {
+                    // same as owner, but can't access user form
                     dashF.Enabled = true;
                     dashF.userButton.Enabled = false;
                     this.Close();
                 }
                 else if (table.Rows[0][5].ToString().Equals("user"))
                 {
+                    // same as admin, but can't access circulation form
                     dashF.Enabled = true;
                     dashF.userButton.Enabled = false;
                     dashF.circulationButton.Enabled = false;
@@ -88,6 +96,7 @@ namespace ManagementSystemProject.FORMS
             }
             else
             {
+                // goes here if username+password combination isn't in the database
                 if(username.Trim().Equals(""))
                 {
                     MessageBox.Show("Enter your username to login.","Empty Username", MessageBoxButtons.OK, MessageBoxIcon.Error);
