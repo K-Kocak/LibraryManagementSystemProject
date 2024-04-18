43c0a931dc18efa753c122632bdaf985d8aca217
K-Kocak <NA>
2024-04-18 22:09:41 +0100

    Author List Form

--- ManagementSystemProject/FORMS/AuthorListForm.cs
+++ ManagementSystemProject/FORMS/AuthorListForm.cs
@@ -17,28 +17,27 @@ namespace ManagementSystemProject.FORMS
         {
             mngBooks = sourceForm as ManageBooksForm;
             InitializeComponent();
-        }
-
-        
+        }     
         private void closeWindow1_Click(object sender, EventArgs e)
         {
+            // close window button
             this.Close();
         }
 
         private void AuthorListForm_Load(object sender, EventArgs e)
         {
+            // gets the author list names from the database and displays them on the UI
             CLASSES.AUTHOR author = new CLASSES.AUTHOR();
             listBoxAuthors.DataSource = author.AuthorList(true);
             listBoxAuthors.DisplayMember = "fullname";
-            listBoxAuthors.ValueMember = "id";
-
-            
+            listBoxAuthors.ValueMember = "id";       
         }
 
         private void selectAuthorAndCloseButton_Click(object sender, EventArgs e)
         {
             try
             {
+                // it will try to fill in the author name box with the selected author with the respective author id
                 DataRowView drv = (DataRowView)listBoxAuthors.SelectedItem;
                 string fullname = drv["fullname"].ToString();
                 string id = drv["id"].ToString();
@@ -53,7 +52,7 @@ namespace ManagementSystemProject.FORMS
             }
             
 
-            
+            // close the pick author form
             this.Close();
         }
     }
