f115668543c38e5ab20b03e7bdcd8938392e208f
K-Kocak <NA>
2024-04-21 18:16:51 +0100

    Books List Form

--- ManagementSystemProject/FORMS/BooksListForm.cs
+++ ManagementSystemProject/FORMS/BooksListForm.cs
@@ -18,6 +18,7 @@ namespace ManagementSystemProject.FORMS
         string fullname;
         public BooksListForm(int id, string Fullname)
         {
+            // Makes the BookListForm (for a particular author) with the passed in authord ID and the Author's full name
             InitializeComponent();
             this.authorID = id;
             this.fullname = Fullname;
@@ -30,32 +31,28 @@ namespace ManagementSystemProject.FORMS
 
         private void BooksListForm_Load(object sender, EventArgs e)
         {
+            // Books by "Author Name"
             labelAuthor.Text = "Books by: " + fullname; 
-
+            // Finds all the books by this particular author ID
             CLASSES.BOOKS book = new CLASSES.BOOKS();
             DataTable bookList = book.authorBooks(authorID);
-
             ListViewItem[] items = new ListViewItem[bookList.Rows.Count];
             String[] titles = new string[bookList.Rows.Count];
-
             for(int i = 0; i < bookList.Rows.Count; i++)
             {
+                // For each book found, we are displaying its book cover with the book title
                 byte[] img = (byte[])bookList.Rows[i][10];
                 MemoryStream ms = new MemoryStream(img);
-
                 imageListBookCovers.Images.Add(Image.FromStream(ms));
-
                 titles[i] = bookList.Rows[i][2].ToString();
-
-
             }
-
+            // Fixing up the book cover image
             listViewBooks.View = View.LargeIcon;
             imageListBookCovers.ImageSize = new Size(200, 250);
             listViewBooks.LargeImageList = imageListBookCovers;
-
             for(int j = 0; j < imageListBookCovers.Images.Count; j++)
             {
+                // Displays the books on the form
                 listViewBooks.Items.Add(new ListViewItem() { Text = titles[j], ImageIndex = j });
             }
         }
