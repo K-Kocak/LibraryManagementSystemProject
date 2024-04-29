1a55fd4727f1da54e554dcc662f3a02bd6846834
K-Kocak <kakocak@outlook.com>
2024-04-29 20:53:35 +0100

    Book Form and Genres

--- ManagementSystemProject/CLASSES/GENRE.cs
+++ ManagementSystemProject/CLASSES/GENRE.cs
@@ -14,6 +14,7 @@ namespace ManagementSystemProject.CLASSES
         THE_DATABASE.MYDB db = new THE_DATABASE.MYDB();
         public bool addGenre(string name)
         {
+            // adds a new genre to the database, id is automatically made
             string query = "INSERT INTO `genres`(`name`) VALUES (@genre_name)"; 
             MySqlParameter[] parameter = new MySqlParameter[1];
             parameter[0] = new MySqlParameter("@genre_name", MySqlDbType.VarChar);
@@ -31,13 +32,13 @@ namespace ManagementSystemProject.CLASSES
 
         public bool editGenre(int id, string name)
         {
+            // update a genre entry by going to the passed-in id and editing the name entry with the given name
             string query = "UPDATE `genres` SET `name`=@genre_name WHERE `id`=@id";
             MySqlParameter[] parameter = new MySqlParameter[2];
             parameter[0] = new MySqlParameter("@genre_name", MySqlDbType.VarChar);
             parameter[0].Value = name;
             parameter[1] = new MySqlParameter("@id", MySqlDbType.Int32);
             parameter[1].Value = id;
-
             if (db.setData(query, parameter) == 1)
             {
                 return true;
@@ -49,12 +50,12 @@ namespace ManagementSystemProject.CLASSES
         }
         public bool removeGenre(int id) 
         {
+            // removes a genre from the table genres with the given id
             string query = "DELETE FROM `genres` WHERE `id`=@id";
             MySqlParameter[] parameter = new MySqlParameter[1];
-
             parameter[0] = new MySqlParameter("@id", MySqlDbType.Int32);
             parameter[0].Value = id;
-
+            // return true if successful, false otherwise
             if (db.setData(query, parameter) == 1)
             {
                 return true;
@@ -66,6 +67,7 @@ namespace ManagementSystemProject.CLASSES
         }
         public DataTable GenresList()
         {
+            // retrieves all the data from the table genres
             DataTable table = new DataTable();
             table = db.getData("SELECT * FROM `genres`", null);
             return table;
--- ManagementSystemProject/FORMS/ManageBooksForm.cs
+++ ManagementSystemProject/FORMS/ManageBooksForm.cs
@@ -14,14 +14,13 @@ namespace ManagementSystemProject.FORMS
 {
     public partial class ManageBooksForm : Form
     {
-        public ManageBooksForm()
-        {
-            InitializeComponent();
-        }
-
         CLASSES.BOOKS book = new CLASSES.BOOKS();
         CLASSES.GENRE genre = new CLASSES.GENRE();
         CLASSES.AUTHOR author = new CLASSES.AUTHOR();
+        public ManageBooksForm()
+        {
+            InitializeComponent();
+        } 
         private void closeWindow1_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -29,13 +28,13 @@ namespace ManagementSystemProject.FORMS
 
         private void ManageBooksForm_Load(object sender, EventArgs e)
         {
+            CLASSES.GENRE genre = new CLASSES.GENRE();
+            // load images onto the form + required data
             addButton1.Image = Image.FromFile("../../IMAGES/addbutton.png");
             editButton1.Image = Image.FromFile("../../IMAGES/editbutton.png");
             removeButton1.Image = Image.FromFile("../../IMAGES/deletebutton.png");
             coverPicturebox.Image = Image.FromFile("../../IMAGES/randombookcover.png");
-            coverEditPictureBox.Image = Image.FromFile("../../IMAGES/randombookcover.png");
-
-            CLASSES.GENRE genre = new CLASSES.GENRE();
+            coverEditPictureBox.Image = Image.FromFile("../../IMAGES/randombookcover.png");        
             genreCombobox.DataSource = genre.GenresList();
             genreCombobox.DisplayMember = "name";
             genreCombobox.ValueMember = "id";
@@ -49,9 +48,9 @@ namespace ManagementSystemProject.FORMS
 
         private void selectCoverButton_Click(object sender, EventArgs e)
         {
+            // user picks the cover of the box by uploading a file, sets this image to the front cover displayed on the form
             OpenFileDialog opf = new OpenFileDialog();
             opf.Filter = "Choose Image(*.jpg;*png;*.gif)|*.jpg;*png;*.gif";
-
             if(opf.ShowDialog() == DialogResult.OK)
             {
                 coverPicturebox.Image = Image.FromFile(opf.FileName);
@@ -61,9 +60,9 @@ namespace ManagementSystemProject.FORMS
 
         private void editPictureCover_Click(object sender, EventArgs e)
         {
+            // same as previous function but triggered from a different button (for editing an existing front cover)
             OpenFileDialog opf = new OpenFileDialog();
             opf.Filter = "Choose Image(*.jpg;*png;*.gif)|*.jpg;*png;*.gif";
-
             if (opf.ShowDialog() == DialogResult.OK)
             {
                 coverEditPictureBox.Image = Image.FromFile(opf.FileName);
@@ -72,18 +71,18 @@ namespace ManagementSystemProject.FORMS
 
         private void selectAuthorButton_Click(object sender, EventArgs e)
         {
+            // display the author list form when the user clicks the select author button
             AuthorListForm autLiFo = new AuthorListForm(this);
             autLiFo.Show();
         }
 
         private void addBookButton_Click(object sender, EventArgs e)
         {
-            
+            // when the user adds a book, the values of inputs are gathered
             int authorID;
             int genreID;
             int quantity;
             double price;
-
             string isbn = ISBNTextbox.Text;
             string title = titleTextbox.Text;
             string publisher = publisherTextbox.Text;
@@ -93,7 +92,6 @@ namespace ManagementSystemProject.FORMS
             MemoryStream ms = new MemoryStream();
             coverPicturebox.Image.Save(ms, coverPicturebox.Image.RawFormat);
             byte[] bookCover = ms.ToArray();
-
             try
             {
                 
@@ -101,15 +99,15 @@ namespace ManagementSystemProject.FORMS
                 genreID = Convert.ToInt32(genreCombobox.SelectedValue);
                 quantity = Convert.ToInt32(numericUpDownQuantity.Value);
                 price = Convert.ToDouble(priceTextbox.Text);
-
                 if(!book.isISBNExists(isbn, 0))
                 {
                     if(!book.isTitleExists(title, 0))
                     {
+                        // if the isbn and the title of the book don't exist, it will try to add this book to the database.
                         if (book.addBook(isbn, title, authorID, genreID, quantity, price, publisher, dateReceived, about, bookCover))
                         {
                             MessageBox.Show("New Book has been added.", "New Book", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
+                            // updates total existing book count
                             bookCountLabel.Text = book.BookList().Rows.Count.ToString() + " Books";
                         }
                         else
@@ -132,12 +130,14 @@ namespace ManagementSystemProject.FORMS
             }
             catch (Exception ex) 
             {
+                // if author id, genre id etc aren't the correct type
                 MessageBox.Show(ex.Message, "Data Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
+            // empties all the text boxes
             foreach(var control in panelAdd.Controls)
             {
                 if(control.GetType() == typeof(TextBox))
@@ -153,6 +153,7 @@ namespace ManagementSystemProject.FORMS
             {
                 theLastBookId = Convert.ToInt32((book.BookList().Rows.Count+1).ToString());
             }
+            // it will fill automatically place the ID of the last book into the ID field upon pressing the clear fields button
             idTextbox.Text = Convert.ToString(theLastBookId);
             aboutRichTextbox.Text = "";
             authorIDbox.Text = "ID";
@@ -164,6 +165,7 @@ namespace ManagementSystemProject.FORMS
 
         private void addButton1_Click(object sender, EventArgs e)
         {
+            // brings the Add panel to the front
             panelAdd.BringToFront();
             authorTextbox.Text = "";
             authorIDbox.Text = "ID:";
@@ -171,6 +173,8 @@ namespace ManagementSystemProject.FORMS
 
         private void editButton1_Click(object sender, EventArgs e)
         {
+            // bring the edit panel to the front
+            // loads up the genre list
             panelEdit.BringToFront();
             genreEditCombobox.DataSource = genre.GenresList();
             genreEditCombobox.DisplayMember = "name";
@@ -183,6 +187,7 @@ namespace ManagementSystemProject.FORMS
 
         private void searchIDButton_Click(object sender, EventArgs e)
         {
+            // searchbyIDbutton, checks if theres a valid ID in the box, then searches for the book
             int id;
             bool validID = Int32.TryParse(IDEditTextbox.Text, out id);
             if (!validID) {
@@ -203,6 +208,7 @@ namespace ManagementSystemProject.FORMS
 
         private void searchISBNButton_Click(object sender, EventArgs e)
         {
+            // same as ID, but searches by isbn instead
             string isbn = ISBNEditTextbox.Text;
             DataTable data = book.searchByIDOrISBN("isbn", 00, isbn);
             if (data.Rows.Count > 0)
@@ -219,24 +225,24 @@ namespace ManagementSystemProject.FORMS
 
         private void selectAuthorEdit_Click(object sender, EventArgs e)
         {
+            // opens the author edit form
             AuthorListForm autLiFo = new AuthorListForm(this);
             autLiFo.Show();
         }
 
         private void editBookButton_Click(object sender, EventArgs e)
         {
+            // similar to add book, but will instead update an existing book when edit book is pressed
             int id;
             int authorID;
             int genreID;
             int quantity;
             double price;
-
             string isbn = ISBNEditTextbox.Text;
             string title = TitleEditTextbox.Text;
             string publisher = publisherEditTextbox.Text;
             string about = EditrichTextbox.Text;
             DateTime dateReceived = editDateTimePicker.Value;
-
             MemoryStream ms = new MemoryStream();
             coverEditPictureBox.Image.Save(ms, coverEditPictureBox.Image.RawFormat);
             byte[] bookCover = ms.ToArray();
@@ -268,14 +274,11 @@ namespace ManagementSystemProject.FORMS
                     {
                         MessageBox.Show("This Book title already exists.", "Title Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
                     }
-
                 }
                 else
                 {
                     MessageBox.Show("ISBN already exists.", "Duplicate ISBN", MessageBoxButtons.OK, MessageBoxIcon.Stop);
                 }
-
-
             }
             catch (Exception ex)
             {
@@ -285,6 +288,7 @@ namespace ManagementSystemProject.FORMS
 
         public void DisplayData(DataTable data)
         {
+            // display retrieved data from table to input fields
             IDEditTextbox.Text = data.Rows[0][0].ToString();
             ISBNEditTextbox.Text = data.Rows[0][1].ToString();
             TitleEditTextbox.Text = data.Rows[0][2].ToString();
@@ -309,46 +313,38 @@ namespace ManagementSystemProject.FORMS
 
         private void showBookListButton_Click(object sender, EventArgs e)
         {
+            // displays a table of all books in the database
             panelShowBooks.BringToFront();
-
             dataGridViewBookList.RowTemplate.Height = 50;
-
             dataGridViewBookList.DataSource = book.BookList();
-
             DataGridViewImageColumn dvgImgCol = new DataGridViewImageColumn();
             dvgImgCol = (DataGridViewImageColumn)dataGridViewBookList.Columns[10];
             dvgImgCol.ImageLayout = DataGridViewImageCellLayout.Stretch;
-
             dataGridViewBookList.ColumnHeadersDefaultCellStyle.ForeColor = Color.Blue;
             dataGridViewBookList.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 10, FontStyle.Bold);
             dataGridViewBookList.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
             dataGridViewBookList.EnableHeadersVisualStyles = false;
-
-            
-
         }
 
         private void removeButton1_Click(object sender, EventArgs e)
         {
+            // shows the book remove form
             RemoveBookForm reBoFo = new RemoveBookForm();
             reBoFo.Show();
         }
 
         private void buttonEditBookData_Click(object sender, EventArgs e)
         {
+            // for editing a specific book, will bring the edit panel to the front
             panelEdit.BringToFront();
-
             genreEditCombobox.DataSource = genre.GenresList();
             genreEditCombobox.DisplayMember = "name";
             genreEditCombobox.ValueMember = "id";
-
             int id = Convert.ToInt32(dataGridViewBookList.CurrentRow.Cells[0].Value);
             DataTable data = book.searchByIDOrISBN("id", id, "");
             if (data.Rows.Count > 0)
             {
                 DisplayData(data);
-
-
             }
             else
             {
@@ -358,46 +354,32 @@ namespace ManagementSystemProject.FORMS
 
         private void buttonExportBooks_Click(object sender, EventArgs e)
         {
-
+            // creates a file which will list the id, isbn and title of all books
             showBookListButton.PerformClick();
-
             if (!Directory.Exists("../books"))
             {
                 Directory.CreateDirectory("../books");
             }
-
             string filePath = "../books/booklist.txt";
-
             if (!File.Exists(filePath))
             {
                 File.Create(filePath).Close();
                 MessageBox.Show("File created.");
             }
-
             TextWriter writer = new StreamWriter(filePath);
-
             string id;
             string isbn;
             string title;
             
-
             for (int i = 0; i < dataGridViewBookList.Rows.Count; i++)
-            {
-                /*for(int j = 0; j < dataGridViewAuthors.Columns.Count; j++)
-                {
-                    writer.Write(dataGridViewAuthors.Rows[i].Cells[j].Value.ToString());
-
-                }*/
-
+            {             
                 id = dataGridViewBookList.Rows[i].Cells[0].Value.ToString();
                 isbn = dataGridViewBookList.Rows[i].Cells[1].Value.ToString();
-                title = dataGridViewBookList.Rows[i].Cells[2].Value.ToString();
-                
+                title = dataGridViewBookList.Rows[i].Cells[2].Value.ToString();            
                 writer.Write(id + " - " + isbn + " - " + title);
                 writer.WriteLine("");
                 writer.WriteLine("-------------------------------------");
             }
-
             writer.Close();
             MessageBox.Show("Data exported.");
         }
@@ -406,6 +388,7 @@ namespace ManagementSystemProject.FORMS
         {
             try
             {
+                //deletes data for a book with a confirmation box
                 int bookID = Convert.ToInt32(dataGridViewBookList.CurrentRow.Cells[0].Value);
                 if (MessageBox.Show("Do you really want to delete this Book?", "Confirmation Box", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
